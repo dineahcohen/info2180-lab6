5af40b65a552aa5516e644b9ad07fd6de29be832
Dineah Cohen <dineahcohen1@gmail.com>
2019-11-11 23:25:57 -0500

    Final

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-var searchBtn= document.getElementById("btn");
-
-searchBtn.onclick = function(){
-    fetch ("superheroes.php").then(response => response.text()).then(data => {
-        alert(data)
-    })
-    .catch(error => {
-        alert(error);
-    });
-}
-
-
-
+window.onload = function(){
+   
+    document.getElementById("btn").onclick = function(){
+      show_result(document.getElementById("queryTxt").value)
+    }
+  }
+  
+  function show_result(res) {  
+    
+    if (window.XMLHttpRequest) {
+      xmlhttpreq=new XMLHttpRequest();
+    } else { 
+      xmlhttpreq=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttpreq.onreadystatechange=function() { 
+      if (this.readyState==4 && this.status==200) {
+        document.getElementById("Results").innerHTML=this.responseText;
+  
+      }
+    }
+    xmlhttpreq.open("GET","superheroes.php ="+res,true);
+    xmlhttpreq.send();
+  }
